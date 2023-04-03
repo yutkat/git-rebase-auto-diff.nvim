55ba09256879210dde14c357e047c60b98f29d41
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2023-04-03 14:40:13 +0900

    Fix scroll

--- lua/git-rebase-auto-diff/init.lua
+++ lua/git-rebase-auto-diff/init.lua
@@ -11,8 +11,8 @@ function M.run(cmd)
 	task_runner = terminal:new({ cmd = cmd, direction = "horizontal", count = 8 })
 	task_runner:open(config.size, "horizontal", true)
 	require("toggleterm.ui").save_window_size()
-	vim.g.toglleterm_win_num = vim.api.nvim_get_current_win()
-	vim.api.nvim_set_option_value("previewwindow", true, { scope = "local", win = vim.g.toglleterm_win_num })
+	vim.g.toglleterm_win_num = vim.fn.winnr()
+	vim.api.nvim_set_option_value("previewwindow", true, { scope = "local", win = vim.api.nvim_get_current_win() })
 	vim.cmd([[stopinsert | wincmd p]])
 end
 
@@ -24,12 +24,25 @@ function M.preview()
 	if hash == "" then
 		return
 	end
-	require("git-rebase-auto-diff").run("git diff " .. hash .. "^!")
+	require("git-rebase-auto-diff").run("git --no-pager diff " .. hash .. "^!")
 end
 
 local function create_autocmds()
 	local group_name = "git-rebase-auto-diff"
 	vim.api.nvim_create_augroup(group_name, { clear = true })
+	vim.api.nvim_create_autocmd({ "TermClose" }, {
+		group = group_name,
+		pattern = "term://*#toggleterm#8*",
+		callback = function()
+			if vim.fn.winbufnr(vim.g.toglleterm_win_num) ~= -1 then
+				vim.cmd(vim.g.toglleterm_win_num .. "wincmd w")
+				vim.cmd("0")
+				vim.cmd("wincmd p")
+			end
+		end,
+		once = false,
+		nested = false,
+	})
 	vim.api.nvim_create_autocmd({ "CursorMoved" }, {
 		group = group_name,
 		pattern = ".git/rebase-merge/git-rebase-todo",
